353d0b8b12eb0b36832e8cc12ae590c0030235c3
James Santos Freitas Azarias <james.azarias@susep.gov.br>
2024-03-02 17:33:41 -0300

    feat: Add initial structure for Ecommerce.Products project
    - Add Domain, Application, Infrastructure, API, and Tests layers;
    - Define basic directory structure for each layer;
    - Prepare the project for future development of product-related functionalities

---
+++ Ecommerce.Products/Ecommerce.Products.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Ecommerce.Products.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
